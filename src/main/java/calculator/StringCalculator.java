a64e6897d1173da8f201e2b28cabbd92563824b4
Jaehee Park <jaeheepark0145@gmail.com>
2020-06-18 22:36:51 +0900

    요구사항 1-4 테스트 코드 작성, 구현

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,6 +2,26 @@ package calculator;
 
 public class StringCalculator {
     int add(String text) {
-        return 0;
+        if (isEmpty(text)) {
+            return 0;
+        }
+
+        String[] strings = text.split(",|:");
+
+        int[] numbers = new int[strings.length];
+        for (int i = 0; i <strings.length ; i++) {
+            numbers[i] = Integer.parseInt(strings[i]);
+        }
+
+        int sum = 0;
+        for (int i = 0; i <numbers.length ; i++) {
+            sum += numbers[i];
+        }
+
+        return sum;
+    }
+
+    private boolean isEmpty(String text) {
+        return text == null || text.isEmpty();
     }
 }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,5 +1,38 @@
 package calculator;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StringCalculatorTest {
+
+    StringCalculator stringCalculator;
+
+    @Before
+    public void before() {
+        stringCalculator = new StringCalculator();
+    }
+
+    @Test
+    public void _01_빈_문자열_또는_null() {
+        assertEquals(0, stringCalculator.add(""));
+        assertEquals(0, stringCalculator.add(null));
+    }
+
+    @Test
+    public void _02_숫자_하나() {
+        assertEquals(1, stringCalculator.add("1"));
+    }
+
+    @Test
+    public void _03_숫자_두개_쉼표() {
+        assertEquals(3, stringCalculator.add("1,2"));
+    }
+
+    @Test
+    public void _04_쉼표랑_콜론() {
+        assertEquals(6, stringCalculator.add("1,2:3"));
+    }
 
 }
