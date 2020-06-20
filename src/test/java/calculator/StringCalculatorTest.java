a35ab9d85b607ab80b890cacf8d3f97d816e4187
Gyumin Kim <starkying@gmail.com>
2020-06-20 14:59:32 +0900

    `@NullAndEmptySource`, `@ParameterizedTest` 적용

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -3,6 +3,8 @@ package calculator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -16,16 +18,14 @@ class StringCalculatorTest {
 		stringCalculator = new StringCalculator();
 	}
 
-	@DisplayName("빈 문자열 또는 null 값을 입력할 경우 0을 반환")
-	@Test
-	void emptyOrNullTextReturnZero() {
+	@NullAndEmptySource
+	@ParameterizedTest
+	void emptyOrNullTextReturnZero(String text) {
 		// when
-		int emptyText = stringCalculator.add("");
-		int nullText = stringCalculator.add(null);
+		int nullOrEmptyText = stringCalculator.add(text);
 
 		// then
-		assertThat(emptyText).isEqualTo(0);
-		assertThat(nullText).isEqualTo(0);
+		assertThat(nullOrEmptyText).isEqualTo(0);
 	}
 
 	@DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환")
